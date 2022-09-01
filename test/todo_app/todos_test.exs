2a2b5658c6eb97d49aa803c8d2eb911c71b5bac6
Jose Juan Reyes <neodevelop@gmail.com>
2022-09-01 18:53:34 -0500

    Add tests for list

--- test/todo_app/todos_test.exs
+++ test/todo_app/todos_test.exs
@@ -58,4 +58,60 @@ defmodule TodoApp.TodosTest do
       assert %Ecto.Changeset{} = Todos.change_task(task)
     end
   end
+
+  describe "lists" do
+    alias TodoApp.Todos.List
+
+    import TodoApp.TodosFixtures
+
+    @invalid_attrs %{name: nil, tags: nil}
+
+    test "list_lists/0 returns all lists" do
+      list = list_fixture()
+      assert Todos.list_lists() == [list]
+    end
+
+    test "get_list!/1 returns the list with given id" do
+      list = list_fixture()
+      assert Todos.get_list!(list.id) == list
+    end
+
+    test "create_list/1 with valid data creates a list" do
+      valid_attrs = %{name: "some name", tags: []}
+
+      assert {:ok, %List{} = list} = Todos.create_list(valid_attrs)
+      assert list.name == "some name"
+      assert list.tags == []
+    end
+
+    test "create_list/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Todos.create_list(@invalid_attrs)
+    end
+
+    test "update_list/2 with valid data updates the list" do
+      list = list_fixture()
+      update_attrs = %{name: "some updated name", tags: []}
+
+      assert {:ok, %List{} = list} = Todos.update_list(list, update_attrs)
+      assert list.name == "some updated name"
+      assert list.tags == []
+    end
+
+    test "update_list/2 with invalid data returns error changeset" do
+      list = list_fixture()
+      assert {:error, %Ecto.Changeset{}} = Todos.update_list(list, @invalid_attrs)
+      assert list == Todos.get_list!(list.id)
+    end
+
+    test "delete_list/1 deletes the list" do
+      list = list_fixture()
+      assert {:ok, %List{}} = Todos.delete_list(list)
+      assert_raise Ecto.NoResultsError, fn -> Todos.get_list!(list.id) end
+    end
+
+    test "change_list/1 returns a list changeset" do
+      list = list_fixture()
+      assert %Ecto.Changeset{} = Todos.change_list(list)
+    end
+  end
 end
