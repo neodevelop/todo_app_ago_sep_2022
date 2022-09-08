15bd7b7ca23161e01166e4da7732558e4aef0c3c
Jose Juan Reyes <neodevelop@gmail.com>
2022-09-08 18:28:47 -0500

    Add a simple live view

---
+++ lib/todo_app_web/live/simple_live.ex
@@ -0,0 +1,9 @@
+defmodule TodoAppWeb.SimpleLive do
+  use Phoenix.LiveView
+
+  def render(assigns) do
+    ~H"""
+    <h1>Hello world!!!</h1>
+    """
+  end
+end
--- lib/todo_app_web/router.ex
+++ lib/todo_app_web/router.ex
@@ -23,6 +23,7 @@ defmodule TodoAppWeb.Router do
     get "/", PageController, :index
     resources "/tasks", TaskController
     resources "/lists", ListController
+    live "/simple", SimpleLive
   end
 
   # Other scopes may use custom stacks.
