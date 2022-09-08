e2676f33387d41d6132213d26865a14b3c50d39b
Jose Juan Reyes <neodevelop@gmail.com>
2022-09-08 18:34:18 -0500

    Add input and capture the keydown event

--- lib/todo_app_web/live/simple_live.ex
+++ lib/todo_app_web/live/simple_live.ex
@@ -4,6 +4,14 @@ defmodule TodoAppWeb.SimpleLive do
   def render(assigns) do
     ~H"""
     <h1>Hello world!!!</h1>
+    <input type="text" name="input" phx-keydown="capture">
+    <label for="text">Result:</label>
+    <h3></h3>
     """
+  end
+
+  def handle_event("capture", params, socket) do
+    IO.inspect(params)
+    {:noreply, socket}
   end
 end
