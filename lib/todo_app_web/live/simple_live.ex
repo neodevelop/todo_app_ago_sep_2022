fc24fa97aaa978cc45181aab70072f9867ec0051
Jose Juan Reyes <neodevelop@gmail.com>
2022-09-08 18:43:54 -0500

    Use template for phx live view

--- lib/todo_app_web/live/simple_live.ex
+++ lib/todo_app_web/live/simple_live.ex
@@ -1,15 +1,6 @@
 defmodule TodoAppWeb.SimpleLive do
   use Phoenix.LiveView
 
-  def render(assigns) do
-    ~H"""
-    <h1>Hello world!!!</h1>
-    <input type="text" name="input" phx-keydown="capture">
-    <label for="text">Result:</label>
-    <h3><%= @message %></h3>
-    """
-  end
-
   def mount(_params, _session, socket) do
     socket = assign(socket, :message, "")
     {:ok, socket}
