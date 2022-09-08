d820ada1921370192fcf42aa88c64f943a8f9f35
Jose Juan Reyes <neodevelop@gmail.com>
2022-09-08 18:41:21 -0500

    Copy value from input to label

--- lib/todo_app_web/live/simple_live.ex
+++ lib/todo_app_web/live/simple_live.ex
@@ -6,12 +6,17 @@ defmodule TodoAppWeb.SimpleLive do
     <h1>Hello world!!!</h1>
     <input type="text" name="input" phx-keydown="capture">
     <label for="text">Result:</label>
-    <h3></h3>
+    <h3><%= @message %></h3>
     """
   end
 
-  def handle_event("capture", params, socket) do
-    IO.inspect(params)
+  def mount(_params, _session, socket) do
+    socket = assign(socket, :message, "")
+    {:ok, socket}
+  end
+
+  def handle_event("capture", %{"key" => _key, "value" => value}, socket) do
+    socket = assign(socket, :message, value)
     {:noreply, socket}
   end
 end
